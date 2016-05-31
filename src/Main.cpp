09a8edf8fcf24b6d3a950371c0f8f8ec5f925b45
Brigham Keys, Esq <bkeys@bkeys.org>
2016-05-30 23:22:27 -0500

    it is not working for some reason

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include <CEGUI/CEGUI.h>
+
+using namespace CEGUI;
 
 int main(int argc, char *argv[]) {
-    std::cout << "Hello world!" << std::endl;
+
+    using namespace CEGUI;
+    Window* myRoot = WindowManager::getSingleton().loadLayoutFromFile( "test.layout" );
+    System::getSingleton().getDefaultGUIContext().setRootWindow( myRoot );
+
+    return 0;
+
+    /*
+    WindowManager& wmgr = WindowManager::getSingleton();
+    Window* myRoot = wmgr.createWindow( "DefaultWindow", "root" );
+    System::getSingleton().getDefaultGUIContext().setRootWindow( myRoot );
+    
+    FrameWindow* fWnd = static_cast<FrameWindow*>(wmgr.createWindow( "TaharezLook/FrameWindow", "testWindow" ));
+    
+    myRoot->addChild( fWnd );
+
+    // position a quarter of the way in from the top-left of parent.
+    fWnd->setPosition( UVector2( UDim( 0.25f, 0.0f ), UDim( 0.25f, 0.0f ) ) );
+    // set size to be half the size of the parent
+    fWnd->setSize(USize(UDim( 0.5f, 0.0f), UDim( 0.5f, 0.0f)));
+    
+    fWnd->setText("Hello World!");
+
+    return 0;    
+    */
 }
