3c2e7691c998e15b1d4f259645e055b9d501d27f
Brigham Keys, Esq <bkeys@bkeys.org>
2016-05-30 22:55:44 -0500

    I got the project set up, everything is dynamically linked and we got a hello world printed

---
+++ build/CMakeFiles/gui.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/bkeys/Devel/CEGUI-Irrlicht/src/Main.cpp" "/home/bkeys/Devel/CEGUI-Irrlicht/build/CMakeFiles/gui.dir/src/Main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "../"
+  "/usr/include/irrlicht"
+  "/usr/include/cegui-0"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/gui.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gui.dir/src/Main.cpp.o"
+  "../bin/gui.pdb"
+  "../bin/gui"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gui.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
